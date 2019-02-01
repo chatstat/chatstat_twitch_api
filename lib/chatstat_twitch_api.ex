aa6ac2af2f8fe8cb2ddb90e21e2ca7ddf484faa9
Norberts Kakste <norberts.kakste@gmail.com>
2019-02-01 21:14:56 +0200

    Delete generated code.

--- lib/chatstat_twitch_api.ex
+++ lib/chatstat_twitch_api.ex
@@ -2,17 +2,4 @@ defmodule TwitchApi do
   @moduledoc """
   Documentation for TwitchApi.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TwitchApi.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/chatstat_twitch_api_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TwitchApiTest do
-  use ExUnit.Case
-  doctest TwitchApi
-
-  test "greets the world" do
-    assert TwitchApi.hello() == :world
-  end
-end
