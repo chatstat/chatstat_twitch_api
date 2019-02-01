a49911cbfc5c8ccf9f4273047d139c3868f3be6f
Norberts Kakste <norberts.kakste@gmail.com>
2019-02-01 21:14:32 +0200

    Initial commit

---
+++ lib/chatstat_twitch_api.ex
@@ -0,0 +1,18 @@
+defmodule TwitchApi do
+  @moduledoc """
+  Documentation for TwitchApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TwitchApi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
